f79ef09247e8a6ec6ac1c58d29ce131c74fdd7ec
mahab-22 <mahab-22@mail.ru>
2021-03-26 13:33:39 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    card_arr: [
+      {
+        image: '1.jpg',
+        country_flag: 'russia.jpg',
+        cost: '2374',
+        addition: `Lorem1 ipsum dolor, sit amet consectetur adipisicing elit. Ea modi ut consequatur minus porro.
+        Dignissimos delectus, modi voluptatum incidunt provident rem sunt laborum repellat debitis,
+        exercitationem voluptates reiciendis officia amet.`
+
+      },
+      {
+        image: '2.jpg',
+        country_flag: 'Russia.jpg',
+        cost: '2545',
+        addition: `Lorem2 ipsum dolor, sit amet consectetur adipisicing elit. Ea modi ut consequatur minus porro.
+        Dignissimos delectus, modi voluptatum incidunt provident rem sunt laborum repellat debitis,
+        exercitationem voluptates reiciendis officia amet.`
+
+      }
+    ]
+
+  },
+  methods: {
+
+
+  }
+
+}
+)
